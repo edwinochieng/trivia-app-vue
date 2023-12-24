dd0db76b819080732f23ef0a360f5e7b82a3c806
edwinochieng <ochiengedwin440@gmail.com>
2023-12-24 14:28:14 +0300

    add dummy data

---
+++ src/data/questions.js
@@ -0,0 +1,52 @@
+export const questions = [
+  {
+    question: "What is the capital of France?",
+    options: [
+      { id: 0, value: "Berlin" },
+      { id: 1, value: "Madrid" },
+      { id: 2, value: "Paris" },
+      { id: 3, value: "Rome" },
+    ],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    options: [
+      { id: 0, value: "Mars" },
+      { id: 1, value: "Jupiter" },
+      { id: 2, value: "Venus" },
+      { id: 3, value: "Saturn" },
+    ],
+    correctAnswer: "Mars",
+  },
+  {
+    question: "What is the largest mammal on Earth?",
+    options: [
+      { id: 0, value: "Elephant" },
+      { id: 1, value: "Blue Whale" },
+      { id: 2, value: "Giraffe" },
+      { id: 3, value: "Hippopotamus" },
+    ],
+    correctAnswer: "Blue Whale",
+  },
+  {
+    question: "Who wrote 'Romeo and Juliet'?",
+    options: [
+      { id: 0, value: "Charles Dickens" },
+      { id: 1, value: "William Shakespeare" },
+      { id: 2, value: "Jane Austen" },
+      { id: 3, value: "Mark Twain" },
+    ],
+    correctAnswer: "William Shakespeare",
+  },
+  {
+    question: "What is the powerhouse of the cell?",
+    options: [
+      { id: 0, value: "Nucleus" },
+      { id: 1, value: "Mitochondria" },
+      { id: 2, value: "Endoplasmic Reticulum" },
+      { id: 3, value: "Golgi Apparatus" },
+    ],
+    correctAnswer: "Mitochondria",
+  },
+];
